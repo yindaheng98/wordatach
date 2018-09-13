0095c6606ae8e05a7820521072928547dee9c3b4
Howard Yin <770275298@qq.com>
2018-09-13 16:24:11 +0800

    delete test

--- test/test.py
+++
@@ -1,7 +0,0 @@
-from wordatas.DataInput import DataInput
-
-data_input = DataInput('斗罗大陆.txt', 10, 10, 0.5)
-print(data_input.next_batch(1))
-print(data_input.next_batch(1))
-print(data_input.next_batch(1))
-print(data_input.next_batch(1))
